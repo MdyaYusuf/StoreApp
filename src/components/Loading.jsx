b5b42e3452734511d50bb288bd630e79dfd18342
MdyaYusuf <myusufunpostakutusu@gmail.com>
2025-06-17 13:54:01 +0300

    Added more details to product pages. Added Loading component and setup axios.

---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import { Backdrop, Box, CircularProgress, Typography } from "@mui/material";
+
+export default function Loading({ message = "Loading..." }) {
+
+  return (
+    <Backdrop open={true} invisible={true}>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <CircularProgress color="primary" />
+        <Typography variant="h4" sx={{ justifyContent: "center", position: "fixed", top: "60%" }}>
+          {message}
+        </Typography>
+      </Box>
+    </Backdrop>
+  )
+}
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import ProductItem from "../components/ProductItem";
+import Loading from "../components/Loading";
+import requests from "../api/apiClient";
+
 export default function ProductDetailsPage() {
-  
+
+  const {id} = useParams();
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    async function fetchProductDetails() {
+      try {
+        const data = await requests.products.details(id);
+        setProduct(data);
+      }
+      catch(error) {
+        console.log(error);
+      }
+      finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProductDetails();
+  }, [id]);
+
+  if (loading) {
+
+    return (
+      <Loading message="Ürün yükleniyor..." />
+    )
+  };
+
   return (
-    <h1>ProductDetailsPage</h1>
-  )
+    <ProductItem product={product} />
+  );
 }
