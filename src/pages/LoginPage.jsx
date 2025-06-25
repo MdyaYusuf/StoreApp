4bdfe2c13639ef5d2ad6b99034639e32504e8a3b
MdyaYusuf <myusufunpostakutusu@gmail.com>
2025-06-25 13:31:48 +0300

    Edited login and register pages.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,41 @@
+import { LockOutlined } from "@mui/icons-material";
+import { Avatar, Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
 export default function LoginPage() {
 
+  const [values, setValues] = useState({
+    username: "",
+    password: ""
+  });
+
+  function handleSubmit(e) {
+    
+    e.preventDefault();
+    console.log(values);
+  }
+
+  function handleInputChange(e) {
+
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  }
+
   return (
-    <h1>LoginPage</h1>
+    <Container maxWidth="xs">
+      <Paper sx={{ padding: 2 }} elevation={3}>
+        <Avatar sx={{ mx: "auto", mb: 2, color: "secondary.main" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5" sx={{ textAlign: "center", mb: 2 }}>
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mb:2 }}>
+          <TextField value={values.username} onChange={handleInputChange} name="username" label="Enter username" size="small" sx={{ mb: 2 }} fullWidth required autoFocus />
+          <TextField value={values.password} onChange={handleInputChange} name="password" type="password" label="Enter password" size="small" sx={{ mb: 2 }} fullWidth required />
+          <Button type="submit" variant="contained" sx={{ mt: 1 }} color="secondary" fullWidth>Submit</Button>
+        </Box>
+      </Paper>
+    </Container>
   )
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,24 @@
+import { LockOutlined } from "@mui/icons-material";
+import { Avatar, Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+
 export default function RegisterPage() {
 
   return (
-    <h1>RegisterPage</h1>
+    <Container maxWidth="xs">
+      <Paper sx={{ padding: 2 }} elevation={3}>
+        <Avatar sx={{ mx: "auto", mb: 2, color: "secondary.main" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5" sx={{ textAlign: "center", mb: 2 }}>
+          Register
+        </Typography>
+        <Box component="form" sx={{ mb:2 }}>
+          <TextField name="username" label="Enter username" size="small" sx={{ mb: 2 }} fullWidth required autoFocus />
+          <TextField name="email" label="Enter email" size="small" sx={{ mb: 2 }} fullWidth required />
+          <TextField name="password" type="password" label="Enter password" size="small" sx={{ mb: 2 }} fullWidth required />
+          <Button type="submit" variant="contained" sx={{ mt: 1 }} color="secondary" fullWidth>Submit</Button>
+        </Box>
+      </Paper>
+    </Container>
   )
 }
